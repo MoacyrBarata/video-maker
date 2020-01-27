cc233218650a8bab5c15571da9372ee15a318c03
Moacyr Barata <moacyrbarata@gmail.com>
2020-01-27 16:23:46 -0400

    feat: building text robot

---
+++ robots/text.js
@@ -0,0 +1,36 @@
+const algorithmia = require('algorithmia')
+const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
+
+async function robot(content){
+    await fetchContentFromWikipedia(content)
+    //sanitizedContent(content)
+    //breakContentIntoSentences(content)
+    async function fetchContentFromWikipedia(content){
+        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+        const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+        const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm)
+        const wikipediaContent = wikipediaResponde.get()
+        
+        content.sourceContentOriginal = wikipediaContent.content
+    }
+
+    function sanitizedContent(content){
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+
+        console.log(withoutBlankLinesAndMarkdown)
+
+        function removeBlankLinesAndMarkdown(text){
+            const allLines = text.split('\n')
+            
+            const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+                if (line.trim().length == 0 || line.trim().startWith('=')){
+                    return false
+                }
+                return true
+            })
+            return withoutBlankLinesAndMarkdown.join(' ')
+        }
+    }
+}
+
+module.exports = robot
